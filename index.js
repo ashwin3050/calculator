c5c6919e0699cf9a688343fa97b7adfbf6196fde
Ashwin <ashwin161408@gmail.com>
2024-06-13 20:53:24 +0530

    initial

---
+++ index.js
@@ -0,0 +1,29 @@
+// calculator
+
+const display = document.getElementById('display');
+
+
+
+function appendToDisplay(input){
+      
+    display.value += input
+
+
+}
+function clearDisplay(){
+     display.value = "";
+
+}
+
+
+function calculator(){
+      
+  try{
+    display.value = eval(display.value)
+
+  }
+    catch{
+        display.value = ' Error'
+    }
+
+}
